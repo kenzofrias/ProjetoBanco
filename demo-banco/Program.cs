26ebea29d78f3e0c27dee0b0a3f8899efdf09e28
Kenzo Friás <kenzofrias16@gmail.com>
2026-05-11 11:42:55 -0300

    feat: adiciona exemplo de uso no demo-banco

---
+++ demo-banco/Program.cs
@@ -0,0 +1,87 @@
+﻿using sistema_de_banco.Models;
+
+// Contas Corrente
+Conta c1 = new ContaCorrente("12345", "João C O Silva", 1200m, 500m);
+Conta c2 = new ContaCorrente("54321", "Maria S O Silva", 1400m, 750m);
+
+// Contas Poupança
+Conta c3 = new ContaPoupanca("67890", "Carlos A O Silva", 2000m, 5m);
+Conta c4 = new ContaPoupanca("09876", "Ana C O Silva", 1500m, 3m);
+
+// Mostrando titulares e saldos
+Console.WriteLine("Titulares e Saldos Iniciais:");
+Console.WriteLine(c1.ToString());
+Console.WriteLine(c2.ToString());
+Console.WriteLine(c3.ToString());
+Console.WriteLine(c4.ToString());
+
+// Realizando operações
+c1.Depositar(100m, 1); // 1300
+c2.Depositar(200m, 1); // 1600
+c3.Depositar(300m, 1); // 2300
+c4.Depositar(400m, 1); // 1900
+
+c1.Sacar(400m); // 900
+c2.Sacar(300m); // 1300
+c3.Sacar(200m); // 2100
+c4.Sacar(100m); // 1800
+
+c1.Transferir(c3, 200m); // C1 transfere 200 reais para C3 - C1: 700, C3: 2300
+c4.Transferir(c2, 400m); // C4 transfere 400 reais para C2 - C4: 1400, C2: 1700
+
+// Mostrando titulares e saldos pós-operações
+Console.WriteLine("\nTitulares e Saldos Pós-Operações:");
+Console.WriteLine(c1.ToString());
+Console.WriteLine(c2.ToString());
+Console.WriteLine(c3.ToString());
+Console.WriteLine(c4.ToString());
+
+// Aplicando cheque especial
+c1.Sacar(1000m); // C1 tenta sacar 1000 - C1: 700 - 1000 = -300 (dentro do limite de cheque especial: 500)
+c2.Sacar(2000m); // C2 tenta sacar 2000 - C2: 1700 - 2000 = -300 (dentro do limite de cheque especial: 750)
+
+// Mostrando titulares e saldos pós-cheque especial (Conta Corrente)
+Console.WriteLine("\nTitulares e Saldos Pós-Cheque Especial(Conta Corrente):");
+Console.WriteLine(c1.ToString());
+Console.WriteLine(c2.ToString());
+
+// Aplicando além do limite de cheque especial
+// c1.Sacar(300m); // Retorna exceção de saldo insuficiente
+// c2.Sacar(500m); // Retorna exceção de saldo insuficiente
+
+// Aplicando rendimento após três meses
+int contador = 1;
+while (contador <= 3)
+{
+    c3.AplicarRendimento(); // Aplica rendimento mensal de 5% para C3
+    c4.AplicarRendimento(); // Aplica rendimento mensal de 3% para C4
+    contador++;
+}
+
+// Mostrando titulares e saldos pós-rendimento (Conta Poupança)
+Console.WriteLine("\nTitulares e Saldos Pós-Rendimento (Conta Poupança):");
+Console.WriteLine(c3.ToString());
+Console.WriteLine(c4.ToString());
+
+// Listando todas as contas
+List<Conta> contas = new List<Conta> { c1, c2, c3, c4 };
+Console.WriteLine("\nListagem de Contas:");
+foreach (var conta in contas)
+{
+    Console.WriteLine(conta.ToString());
+}
+
+// Exibindo extratos individuais
+Console.WriteLine("\n");
+c1.ExibirExtrato();
+
+Console.WriteLine("\n");
+c2.ExibirExtrato();
+
+Console.WriteLine("\n");
+c3.ExibirExtrato();
+
+Console.WriteLine("\n");
+c4.ExibirExtrato();
+
+Console.WriteLine("\n");
